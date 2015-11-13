b0bb3876a394986796937d259516623aca69a363
jose Ramon Bogarin <bogarin617@gmail.com>
2015-11-12 19:56:37 -0800

    modificaciones

--- app/src/main/java/com/example/bogarin/crud/InfoDB.java
+++ app/src/main/java/com/example/bogarin/crud/InfoDB.java
@@ -1,5 +1,8 @@
 package com.example.bogarin.crud;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
@@ -12,6 +15,7 @@ public class InfoDB {
     public static final String STRING_TYPE="text";
     public static final String INT_TYPE= "integer";
     public static String readDatos;
+    ContentValues valores;
 
     public static class EncavezadoAlumnos{
         public static final String ID_ALUMNO= BaseColumns._ID;
@@ -32,5 +36,46 @@ public class InfoDB {
         readDatos ="inser into"+TABLE_NAME+" values("+"null,"+name1+","+name2+","+apellidop+","+apellidom+","+fecha+")";
     }
 
+    private BasedeDatos abrirHelper;
+    private SQLiteDatabase db;
+
+    public InfoDB(Context contexto){
+        //creando la instancia de la base de datos
+        abrirHelper=new InfoDB(contexto);
+        db=abrirHelper.getWritableDatabase();
+        valores=new ContentValues();
+    }
+
+    public void saveAlumnosRow(String name1,String name2,String apellidop,String apellidom, String fecha){
+        valores=null;
+        valores.put(EncavezadoAlumnos.NAME1,name1);
+        valores.put(EncavezadoAlumnos.NAME2,name2);
+        valores.put(EncavezadoAlumnos.APELLIDOP,apellidop);
+        valores.put(EncavezadoAlumnos.APELLIDOM,apellidom);
+        valores.put(EncavezadoAlumnos.FECHA_NAC,fecha);
+        db.insert(TABLE_NAME,null,valores);
+    }
+
+    public Cursor getAllAlumnos(){
+        return db.rawQuery("select * from "+TABLE_NAME,null);
+    }
+
+    public void deleteAllAlumnos(){
+        db.delete(TABLE_NAME,null,null);
+    }
+
+    public void deleteNombreAlumnos(String condicion){
+        db.delete(TABLE_NAME,EncavezadoAlumnos.NAME1+"="+condicion,null);
+    }
+
+    public void updateAlumnos(String name1,String name2,String apellidop,String apellidom, String fecha){
+        valores=null;
+        valores.put(EncavezadoAlumnos.NAME1,name1);
+        valores.put(EncavezadoAlumnos.NAME2,name2);
+        valores.put(EncavezadoAlumnos.APELLIDOP,apellidop);
+        valores.put(EncavezadoAlumnos.APELLIDOM,apellidom);
+        valores.put(EncavezadoAlumnos.FECHA_NAC,fecha);
+        db.update(TABLE_NAME,valores,EncavezadoAlumnos.NAME1+"="+name1,null);
+    }
 
 }
